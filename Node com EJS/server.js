1b847f20850e5f4c935101a69662936ab85a5fe7
Romeu Rezende <romeu_0104@hotmail.com>
2022-06-01 21:52:50 -0300

    Node+EJS-objetos do JS para HTML - forEach

--- Node com EJS/server.js
+++ Node com EJS/server.js
@@ -4,7 +4,34 @@ const app = express()
 app.set("view engine","ejs")// set é um método do módulo express view engine é a ferramenta utilizada para renderizar Html será o ejs
 
 app.get("/", function (req, res){//Criando uma rota o "/" na verdade seria o domínio principal do site
-    res.render("pages/index")//quando requisitar (req) o domínio principal a resposta (res) será renderizar o index.ejs
+     
+    //passar um objeto do JavaScript para para a página Html, no caso aqui ele está vinculado ao render do index, por isso só aparecerá lá
+    const items=[
+        {
+            title: "D",
+            message: "Diníozio" 
+        },
+        {
+            title: "E",
+            message: "Enésimo" 
+        },
+        {
+            title: "U",
+            message: "Uelinton" 
+        },
+        {
+            title: "S",
+            message: "Sabrina" 
+        },
+        
+    ];
+    const subtitle="Qualquer coisa aqui"
+
+    //abaixo no render passo esse objeto com o nome definido
+    res.render("pages/index", {
+        nomeObjetoPlural: items,
+        subtitle: subtitle
+    })//quando requisitar (req) o domínio principal a resposta (res) será renderizar o index.ejs
 }) 
 
 //exemplo de como seria uma sub página do domínio principal
